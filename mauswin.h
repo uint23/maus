64e4683cd96a8c9b37328907ce06b6da8a86060a
seiko <uint@fent.uk>
2026-06-02 16:19:43 +0100

    Add mauswin.h, boilerplate, init function, logging

---
+++ mauswin.h
@@ -0,0 +1,42 @@
+#ifndef MAUSWIN_H
+#define MAUSWIN_H
+
+#include <stdio.h>
+
+/* auto selection */
+#if !defined(BACKEND_WIN) && !defined(BACKEND_MAC) && \
+    !defined(BACKEND_X11) && !defined(BACKEND_WAY)
+
+#define MAUS_WARN_BACKEND_AUTO_SEL 1
+
+#if defined(_WIN32)
+#define BACKEND_WIN32
+
+#elif defined(__APPLE__)
+#define BACKEND_COCOA
+
+#else /* default to X not Way */
+#define BACKEND_X11
+
+#endif
+#endif /* auto selection */
+
+#if defined(BACKEND_X11)
+#include "mauswin_x11.h"
+
+#elif defined(BACKEND_WAY)
+/* ... */
+
+#elif defined(BACKEND_WIN)
+/* ... */
+
+#elif defined(BACKEND_MAC)
+/* ... */
+
+#endif
+
+MausWindow* maus_init(const char* title, int width, int height);
+void maus_log(FILE* fd, const char* fmt, ...);
+
+#endif /* MAUSWIN_H */
+
