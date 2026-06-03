bf34812d37ffbdf4d67207bb1b60fefa6320631e
uint <uint@fent.uk>
2026-06-03 19:51:55 +0100

    Update basic-window to also poll for events

--- tests/basic-window/main.c
+++ tests/basic-window/main.c
@@ -1,14 +1,39 @@
-#define BACKEND_X11
+#include <stdlib.h>
+
 #include "../../maus.h"
+
+void handle_ev(MausEvent* ev, Maus* mw)
+{
+	switch (ev->type) {
+		case MAUS_EV_CLOSE:
+			maus_close(mw);
+			exit(EXIT_SUCCESS);
+		case MAUS_EV_KEY:
+			if (ev->key.code == 24) { /* quit */
+				maus_close(mw);
+				exit(EXIT_SUCCESS);
+			}
+		case MAUS_EV_MOUSE_BUTTON:
+			break;
+		case MAUS_EV_MOUSE_MOTION:
+			break;
+		case MAUS_EV_RESIZE:
+			break;
+		case MAUS_EV_NONE:
+			break;
+	}
+}
 
 int main(void)
 {
 	Maus* mw = maus_init("basic window", 0, 0, 800, 600);
 	maus_create_window(mw);
 
-	XEvent e;
-	for (;;)
-		XNextEvent(mw->display, &e);
+	MausEvent ev;
+	for (;;) {
+		while (maus_poll(mw, &ev))
+			handle_ev(&ev, mw);
+	}
 
 	maus_close(mw);
 }
